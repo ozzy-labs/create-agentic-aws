ad9cbc905645a7415f2df5f54680ed026c7d43de
ozzy-3 <104637034+ozzy-3@users.noreply.github.com>
2026-03-28 18:04:36 +0900

    fix: add Lambda integration and route to Terraform API Gateway template (#153)

--- src/presets/api-gateway.ts
+++ src/presets/api-gateway.ts
@@ -17,6 +17,30 @@ resource "aws_apigatewayv2_stage" "default" {
   name        = "$default"
   auto_deploy = true
 }
+
+# --- Lambda integration ---
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id                 = aws_apigatewayv2_api.this.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.this.invoke_arn
+  integration_method     = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "default" {
+  api_id    = aws_apigatewayv2_api.this.id
+  route_key = "$default"
+  target    = "integrations/\${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id  = "AllowAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "\${aws_apigatewayv2_api.this.execution_arn}/*/*"
+}
 `;
 
 const API_GATEWAY_TF_OUTPUTS = `output "api_gateway_url" {

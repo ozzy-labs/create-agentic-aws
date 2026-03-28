92fc74e7c141efdbfe27faf4143690c65234b8b1
ozzy-3 <yosuke.ojima@gmail.com>
2026-03-28 17:59:18 +0900

    fix: use S3 bucket resource reference in Terraform CloudFront template

--- src/presets/cloudfront.ts
+++ src/presets/cloudfront.ts
@@ -12,7 +12,7 @@ resource "aws_cloudfront_distribution" "this" {
   default_root_object = "index.html"
 
   origin {
-    domain_name              = "\${var.project_name}-origin.s3.amazonaws.com"
+    domain_name              = aws_s3_bucket.this.bucket_regional_domain_name
     origin_id                = "s3-origin"
     origin_access_control_id = aws_cloudfront_origin_access_control.this.id
   }
@@ -41,6 +41,26 @@ resource "aws_cloudfront_distribution" "this" {
       restriction_type = "none"
     }
   }
+}
+
+resource "aws_s3_bucket_policy" "cloudfront" {
+  bucket = aws_s3_bucket.this.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Sid       = "AllowCloudFrontOAC"
+      Effect    = "Allow"
+      Principal = { Service = "cloudfront.amazonaws.com" }
+      Action    = "s3:GetObject"
+      Resource  = "\${aws_s3_bucket.this.arn}/*"
+      Condition = {
+        StringEquals = {
+          "AWS:SourceArn" = aws_cloudfront_distribution.this.arn
+        }
+      }
+    }]
+  })
 }
 `;
 
